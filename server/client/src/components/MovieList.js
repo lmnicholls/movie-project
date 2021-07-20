f2be7de37ecb3de39ee709d2f47b3ea8d5ffb486
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-07-20 10:46:05 -0400

    added infinite scrolling

--- server/client/src/components/MovieList.js
+++ server/client/src/components/MovieList.js
@@ -1,37 +1,55 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Movie from "./Movie";
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchMovies } from '../actions';
+import { useDispatch, useSelector } from "react-redux";
+import InfiniteScroll from "react-infinite-scroller";
+import { fetchMovies } from "../actions";
 
 const MovieList = () => {
-  const movieOrder = useSelector(state => state.movies.order);
-  const movies = useSelector(state => state.movies.entries);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+  const movieOrder = useSelector((state) => state.movies.order);
+  const movies = useSelector((state) => state.movies.entries);
+  const totalPages = useSelector((state) => state.total_pages);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchMovies());
   }, [dispatch]);
 
+  const loadItems = (page) => {
+    if (page < totalPages || totalPages === 0) {
+      dispatch(fetchMovies(page));
+    } else {
+      setHasMoreItems(false);
+    }
+  };
+
   const movieComponents = movieOrder.map((id) => {
     const movie = movies[id];
 
-    return <Movie id={movie.id} key={id} title={movie.title} img={movie.poster_path} />
+    return (
+      <Movie
+        id={movie.id}
+        key={id}
+        title={movie.title}
+        img={movie.poster_path}
+      />
+    );
   });
 
   return (
-    <MovieGrid>
-      {movieComponents}
-    </MovieGrid>
-  )
-}
+    <InfiniteScroll loadMore={loadItems} pageStart={0} hasMore={hasMoreItems}>
+      <MovieGrid>{movieComponents}</MovieGrid>
+    </InfiniteScroll>
+  );
+};
 
 export default MovieList;
 
 const MovieGrid = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: right;
   flex-wrap: wrap;
   padding: 2em;
   margin: 0 auto;
--- server/client/src/reducers/reducer-movies.js
+++ server/client/src/reducers/reducer-movies.js
@@ -1,30 +1,34 @@
-import { FETCH_MOVIES, FETCH_MOVIE } from '../actions/types';
-import { normalize, schema } from 'normalizr';
-import _ from 'lodash';
+import { FETCH_MOVIES, FETCH_MOVIE } from "../actions/types";
+import { normalize, schema } from "normalizr";
+import _ from "lodash";
 
 const DEFAULT_STATE = {
   entries: {},
-  order: []
-}
+  order: [],
+};
 
-const moviesSchema = new schema.Entity('movies', undefined);
+const moviesSchema = new schema.Entity("movies", undefined);
 
-export default function(state = DEFAULT_STATE, action) {
+export default function (state = DEFAULT_STATE, action) {
   switch (action.type) {
     case FETCH_MOVIES:
-      const normalizedMovies = normalize(action.payload.results, [moviesSchema]);
+      const normalizedMovies = normalize(action.payload.results, [
+        moviesSchema,
+      ]);
 
       return {
         // uniq ensures that if a movie was added with FETCH_MOVIE it
         // won't show up twice in the list
+        // QUESTION - Is there a better way to do this? When you click on
+        // a movie it makes a duplicate of that move...
         order: _.uniq([...state.order, ...normalizedMovies.result]),
-        entries: { ...normalizedMovies.entities.movies, ...state.entries }
-      }
+        entries: { ...normalizedMovies.entities.movies, ...state.entries },
+      };
     case FETCH_MOVIE:
-      return { 
-        order: _.union([...state.order], [action.payload.id]), 
-        entries: { ...state.entries, [action.payload.id]: action.payload }
-      }
+      return {
+        order: _.union([...state.order], [action.payload.id]),
+        entries: { ...state.entries, [action.payload.id]: action.payload },
+      };
     default:
       return state;
   }
